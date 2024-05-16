65d4963b7ef16529bd7711f32baa89ddb9ab0a69
christian_santana <crsantanarj@hotmail.com>
2024-05-16 11:19:01 -0300

    fix: Modificando arquivo vite.config.js para atualização do deploy

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -3,77 +3,77 @@ export const projects = [
     name: 'Kenzie Movie',
     description: 'Aplicação voltada para avaliação de filmes',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/kenzie-movie-entrega-Grupo8',
-    image: 'src/assets/photo_project/kenzie_movie_image.jpg',
+    image: '/src/assets/photo_project/kenzie_movie_image.jpg',
     link_app: 'https://kenzie-academy-brasil-developers-kenzie-academy-allandoalmeida.vercel.app/'
   },
   {
     name: 'Open Music',
     description: 'Aplicação voltada para compras de albums de música',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/Kenzie-Academy-Brasil-Developers-open-music-base_christian1518',
-    image: 'src/assets/photo_project/open_music_photo.jpg',
+    image: '/src/assets/photo_project/open_music_photo.jpg',
     link_app: 'https://open-music-79bepnjjk-christian1518s-projects.vercel.app/'
   },
   {
     name: 'Controll Finance',
     description: 'Aplicação voltada para o gerenciamento de entrada e sáida de valores de uma empresa',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/Kenzie-Academy-Brasil-Developers-control-finance_christian1518',
-    image: 'src/assets/photo_project/controll_finance_photo.jpg',
+    image: '/src/assets/photo_project/controll_finance_photo.jpg',
     link_app: 'https://control-finance-736ys42jh-christian1518s-projects.vercel.app/'
   },
   {
     name: 'Hamburgueria',
     description: 'Aplicação voltada para compra de produtos fornecidos por uma hamburgueria',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/-Kenzie-Academy-Brasil-Developers-react-entrega-s3-template-hamburgueria_christian1518',
-    image: 'src/assets/photo_project/hamburgueria_image.jpg',
+    image: '/src/assets/photo_project/hamburgueria_image.jpg',
     link_app: 'https://burguerkenzie-pi.vercel.app/'
   },
   {
     name: 'Git Search',
     description: 'Aplicação voltada para busca de usuários no github',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/Kenzie-Academy-Brasil-Developers-gitSearchBase__christian1518',
-    image: 'src/assets/photo_project/git_search_photo.jpg',
+    image: '/src/assets/photo_project/git_search_photo.jpg',
     link_app: 'https://git-search-r53adsbxy-christian1518s-projects.vercel.app/index.html'
   },
   {
     name: 'BandKamp',
     description: 'API voltada para cadastro de albums e músicas',
     repository: 'https://github.com/christian1518/m5-bandkamp-generic-view_christian1518',
-    image: 'src/assets/photo_project/bandkamp_photo.jpg',
+    image: '/src/assets/photo_project/bandkamp_photo.jpg',
     link_app: 'https://bandkamp-app-12sk.onrender.com/api/docs/'
   },
   {
     name: 'Ecommerce',
     description: 'Aplicação simples de um ecommerce',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/Kenzie-Academy-Brasil-Developers-M1-Ecommerce_christian1518',
-    image: 'src/assets/photo_project/ecommerce_photo.jpg',
+    image: '/src/assets/photo_project/ecommerce_photo.jpg',
     link_app: 'https://ecommerce-id4y3scb7-christian1518s-projects.vercel.app'
   },
   {
     name: 'Site genérico',
     description: 'Aplicação de um site genérico',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/-Kenzie-Academy-Brasil-Developers-m2-criando-site-institucional-generico_christian1518',
-    image: 'src/assets/photo_project/site_generico.jpg',
+    image: '/src/assets/photo_project/site_generico.jpg',
     link_app: 'https://sitegeneric-idpjaumfg-christian1518s-projects.vercel.app'
   },
   {
     name: 'Nukenzie',
     description: 'Aplicação para controle de finanças',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/-Kenzie-Academy-Brasil-Developers-react-entrega-s1-template-nu-kenzie_christian1518',
-    image: 'src/assets/photo_project/nukenzie_photo.jpg',
+    image: '/src/assets/photo_project/nukenzie_photo.jpg',
     link_app: 'https://nukenzie-christian1518.vercel.app/'
   },
   {
     name: 'KenzieHub',
     description: 'Aplicação voltada para cadastro e login do usuário',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/-Kenzie-Academy-Brasil-Developers-react-entrega-kenzie-hub_christian1518',
-    image: 'src/assets/photo_project/kenziehub_photo.jpg',
+    image: '/src/assets/photo_project/kenziehub_photo.jpg',
     link_app: 'https://kenzie-academy-brasil-developers-react-entrega-ke-christian1518.vercel.app/'
   },
   {
     name: 'Portfólio Genérigo',
     description: 'Modelo genérico de portfólio',
     repository: 'https://github.com/Kenzie-Academy-Brasil-Developers/-Kenzie-Academy-Brasil-Developers-m3-s1-entrega-portfolio-template_christian1518',
-    image: 'src/assets/photo_project/portfolio_generico_photo.jpg',
+    image: '/src/assets/photo_project/portfolio_generico_photo.jpg',
     link_app: 'https://portfolio-seven-nu-85.vercel.app/'
   },
 ];
