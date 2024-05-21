9f53d837b0ff20f92f3d7f94198cc7dc8a7e3dd6
christian_santana <crsantanarj@hotmail.com>
2024-05-21 15:12:46 -0300

    fix: Útima atualização

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg']
+  // assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg']
 })
